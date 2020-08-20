176f028b569df7ef787833b093ddf2b32db69c71
Lucas Copes <50839665+Kimmalyn@users.noreply.github.com>
2020-08-20 09:08:42 -0300

    Creacion venv
    creacion de entorno virtual con virtualenv

--- tp_iw/manage.py
+++ tp_iw/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tpiw.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tp_iw.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
