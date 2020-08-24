fd146be50b1860045ecae471cbb2a46decd5036e
Lucas Copes <50839665+Kimmalyn@users.noreply.github.com>
2020-08-24 10:18:41 -0300

    Creacion de carpetas apps
    Movimos las apps dentro de un directorio con ese mismo nombre

--- tp_iw/tp_iw/urls.py
+++ tp_iw/tp_iw/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from usuario.views import login_user, home, register, home_logueado, logout_user
+from apps.usuario.views import login_user, home, register, home_logueado, logout_user
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
